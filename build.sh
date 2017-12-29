24ec42a65c7b2a4b65f4fcd65cd00d007ed29b91
Erwin de Keijzer <erwin@dekeijzer.xyz>
2017-12-29 13:05:26 +0100

    ci tools added

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-dep ensure
-go-bindata-assetfs static/...;
-go build -o booksing *.go
+if [[ "${1}" != "release" ]]; then
+    go-bindata-assetfs static/...;
+    go build -o booksing *.go
+    exit 0
+fi
+
+for os in linux darwin windows; do
+    for arch in 386 arm amd64; do
+        CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o "dist/booksing-$os-$arch" -a -ldflags '-extldflags "-static"' .
+    done
+done
