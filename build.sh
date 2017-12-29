30bcb776a1f120eb27d2b50386b9ea958f2e0554
Erwin de Keijzer <erwin@dekeijzer.xyz>
2017-12-29 13:47:47 +0100

    speed up builds

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [[ "${1}" != "release" ]]; then
+if [[ "${1}" == "test" ]]; then
+    go build -o booksing *.go
+
+elif [[ "${1}" != "release" ]]; then
     go-bindata-assetfs static/...;
     go build -o booksing *.go
-    exit 0
-fi
+else
 
-for os in linux darwin windows; do
-    for arch in 386 arm amd64; do
-        echo "building $os $arch"
-        CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o "~/dist/booksing-$os-$arch" -a -ldflags '-extldflags "-static"' .
+    for os in linux darwin windows; do
+        for arch in 386 arm amd64; do
+            echo "building $os $arch"
+            CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o "~/dist/booksing-$os-$arch" -a -ldflags '-extldflags "-static"' .
+        done
     done
-done
+
+fi
